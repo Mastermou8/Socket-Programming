9b4a7df136653f425310c1e0891f26dfdd7bd2de
Mastermou8 <mouperalta@gmail.com>
2025-11-12 15:22:43 -0500

    Client thingy

--- client.py
+++ client.py
@@ -1,12 +1,41 @@
-#This file is for the client
+import os
 import socket
 
-def Main():
+# IP = "192.168.1.101" #"localhost"
+IP = "192.168.131.8"
+PORT = 4450
+ADDR = (IP, PORT)
+SIZE = 1024  ## byte .. buffer size
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
 
-    # add host here->
-    #host = ''
-    #port =
 
-    #socket object for network communication
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    while True:  ### multiple communications
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
+        if cmd == "OK":
+            print(f"{msg}")
+        elif cmd == "DISCONNECTED":
+            print(f"{msg}")
+            break
 
+        data = input("> ")
+        data = data.split(" ")
+        cmd = data[0]
+
+        if cmd == "TASK":
+            client.send(cmd.encode(FORMAT))
+
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+
+    print("Disconnected from the server.")
+    client.close()  ## close the connection
+
+
+if __name__ == "__main__":
+    main()
