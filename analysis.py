83f737d19fb76ef56428682a816e2fea2ead9a07
mabric <mabriccorcuera@gmail.com>
2025-11-12 17:46:21 -0500

    Started

--- analysis.py
+++ analysis.py
@@ -1,1 +1,9 @@
-#This file will colect and analyze Upload & Download rates, etc...
+import os
+import socket
+
+IP = "192.168.131.8"
+PORT = 4450
+ADDR = (IP, PORT)
+SIZE = 1024  ## byte .. buffer size
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
