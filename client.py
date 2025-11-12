81fa28244b58759aa6556bbb1213febd013272af
Mastermou8 <mouperalta@gmail.com>
2025-11-12 15:00:16 -0500

    Created the socket object line

--- client.py
+++ client.py
@@ -1,1 +1,12 @@
 #This file is for the client
+import socket
+
+def Main():
+
+    # add host here->
+    #host = ''
+    #port =
+
+    #socket object for network communication
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
